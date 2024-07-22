167a09d2a2c265feabdaaccb62fdaa9d7d683788
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-07-22 09:04:37 +0100

    Add config for documentation

--- app/main.py
+++ app/main.py
@@ -1,14 +1,9 @@
 from fastapi import FastAPI
 from .routers import case_information
-import os
+from .config import config
 
 
 def create_app():
-    app = FastAPI()
+    app = FastAPI(**config)
     app.include_router(case_information.router)
     return app
-
-
-if __name__ == "__main__":
-    cwd = os.getcwd()
-    os.chdir("./app")

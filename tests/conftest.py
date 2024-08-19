3670649ad8fd3282506a4aa452a6ea51c07b0d7c
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2024-08-19 14:31:02 +0100

    Bypass auth for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 import pytest
 from sqlmodel import SQLModel, create_engine, Session, StaticPool
 from app import case_api
+from app.auth.security import get_current_user
 from app.db import get_session
 from fastapi.testclient import TestClient
 
+from app.models import Users
 
 @pytest.fixture(name="session")
 def session_fixture():
@@ -19,8 +21,14 @@ def session_fixture():
 def client_fixture(session: Session):
     def get_session_override():
         return session
+    
+    def override_get_current_user():
+        # Return a dummy user or None to bypass authentication
+        return Users(username="testuser", hashed_password="dummyhash")
 
     case_api.dependency_overrides[get_session] = get_session_override
+
+    case_api.dependency_overrides[get_current_user] = override_get_current_user
 
     client = TestClient(case_api)
     yield client

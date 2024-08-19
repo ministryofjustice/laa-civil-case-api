703ff28d1d8ea01c2bec055d8b1331c1531e6188
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2024-08-19 09:06:37 +0100

    Change file structure

--- app/auth/security.py
+++ app/auth/security.py
@@ -1,26 +1,38 @@
-from typing import Union
+from typing import Union, Annotated
+from datetime import timezone, timedelta, datetime
+
 from passlib.context import CryptContext
-from jose import JWTError, jwt
+import jwt
+from jwt.exceptions import InvalidTokenError
 from datetime import datetime, timedelta
-from fastapi.security import OAuth2PasswordBearer
-from fastapi import HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import HTTPException, Depends, status, FastAPI
+from pydantic import BaseModel
+import app
 
 # Change secret key to K8 secret
-SECRET_KEY = "your-secret-key"
+SECRET_KEY = "6c6b04c74f00966489fa5df26cb21ee98d325f0cd3f5c763c980f740dcd1a777"
 ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
+class UserInDB(User):
+    hashed_password: str
 
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-# Fake user database for testing
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "hashed_password": pwd_context.hash("password"),
-        "disabled": False,
-    }
-}
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -28,29 +40,31 @@ def verify_password(plain_password, hashed_password):
 def get_password_hash(password):
     return pwd_context.hash(password)
 
-def get_user(username: str):
-    user = fake_users_db.get(username)
-    if user:
-        return user
-    return None
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
 
-def authenticate_user(username: str, password: str):
-    user = get_user(username)
-    if not user or not verify_password(password, user['hashed_password']):
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
         return False
     return user
 
 def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(timezone.gmt) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now(timezone.gmt) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -61,9 +75,17 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-        user = get_user(username)
-        if user is None:
-            raise credentials_exception
-    except JWTError:
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
         raise credentials_exception
     return user
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
--- app/main.py
+++ app/main.py
@@ -1,33 +1,46 @@
+from datetime import timedelta
+from typing import Annotated
+
 from fastapi import FastAPI, Depends, HTTPException, status
 from .routers import case_information
 from .config.docs import config as docs_config
 from .auth.security import create_access_token, get_current_user
 from fastapi.security import OAuth2PasswordRequestForm
-from .auth.models import User, Token
-from .auth.security import authenticate_user
-from datetime import timedelta
+from .auth.security import User, Token, authenticate_user, ACCESS_TOKEN_EXPIRE_MINUTES
 
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
 
 def create_app():
     app = FastAPI(**docs_config)
     app.include_router(case_information.router)
 
     # Defines the token endpoint
-    @app.post("/token", response_model=Token)
-    async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-        user = authenticate_user(form_data.username, form_data.password)
+    @app.post("/token")
+    async def login_for_access_token(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    ) -> Token:
+        user = authenticate_user(fake_users_db, form_data.username, form_data.password)
         if not user:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Incorrect username or password",
                 headers={"WWW-Authenticate": "Bearer"},
             )
-        access_token_expires = timedelta(minutes=30)
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
         access_token = create_access_token(
             data={"sub": user.username}, expires_delta=access_token_expires
         )
-        return {"access_token": access_token, "token_type": "bearer"}
-
+        return Token(access_token=access_token, token_type="bearer")
+    
     @app.get("/users/me", response_model=User)
     async def read_users_me(current_user: User = Depends(get_current_user)):
         return current_user

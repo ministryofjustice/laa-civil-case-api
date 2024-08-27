e1b3e4e79b28270d40b45115a809289f8a29958c
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2024-08-27 11:47:20 +0100

    Update test coverage

--- tests/auth/test_auth.py
+++ tests/auth/test_auth.py
@@ -1,10 +1,10 @@
 from fastapi.testclient import TestClient
-from app.auth.security import create_access_token, verify_password, get_password_hash
+from app.auth.security import create_access_token, verify_password, get_password_hash, authenticate_user, get_current_active_user, ACCESS_TOKEN_EXPIRE_MINUTES
 from freezegun import freeze_time
 import pytest
 from jwt import ExpiredSignatureError
-from datetime import timedelta
-
+from datetime import timedelta, datetime
+from app.models.users import Users
 
 def test_auth_fail_case(client: TestClient):
     response = client.post(
@@ -45,6 +45,21 @@ def test_credential_exception(client: TestClient, auth_token):
     assert json["detail"] == 'Could not validate credentials'
     assert response.status_code == 401
 
+def test_credential_exception_no_user(session, client: TestClient, auth_token):
+    username = "cla_admin"
+    user = session.get(Users, username)
+    session.delete(user)
+    session.commit()
+    response = client.get(
+        "/cases/", headers={"Authorization": f"Bearer {auth_token}"})
+    json = response.json()
+    assert json["detail"] == 'Could not validate credentials'
+    assert response.status_code == 401
+
+def test_authenticate_user(session):
+    auth_user = authenticate_user(session=session, username='cla_admin', password='incorrect')
+    assert auth_user == False
+
 def test_password_hashing():
     hashed_password = 'password'
     assert verify_password('password', get_password_hash(hashed_password))
@@ -53,10 +68,19 @@ def test_create_token():
     jwt = create_access_token(data={"sub": 'cla_admin'}, expires_delta=timedelta(minutes=30))
     assert len(jwt) == 129
 
-def test_token_expiry():
+def test_token_with_no_expire():
     with freeze_time("2024-08-23 10:00:00"):
-        encoded_jwt = create_access_token(data={"sub": 'cla_admin'}, expires_delta=timedelta(minutes=1))
-    assert encoded_jwt is not None
+        token = create_access_token(data={"sub": 'cla_admin'})
+        new_time = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES + 10)
+    assert token is not None
+
+    with freeze_time(new_time):
+        assert pytest.raises(ExpiredSignatureError)
+
+def test_token_defined_expiry():
+    with freeze_time("2024-08-23 10:00:00"):
+        token = create_access_token(data={"sub": 'cla_admin'}, expires_delta=timedelta(minutes=1))
+    assert token is not None
 
     with freeze_time("2024-08-23 10:05:00"):
         assert pytest.raises(ExpiredSignatureError)

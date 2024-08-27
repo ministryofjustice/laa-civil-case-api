e448f448c2ecabfb4e1b5bf99fa04da2f90ebe99
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2024-08-27 15:56:31 +0100

    Move secret key

--- run_local.sh
+++ run_local.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ -z ${SECRET_KEY} ]; then
-    echo "SECRET_KEY is unset."
-    exit 1
-fi
-
 docker compose down --remove-orphans
 docker compose up -d --build
 docker compose wait migrations

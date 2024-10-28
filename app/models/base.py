78f72bff87ca90216687ba393469eaa756054157
said-moj <45761276+said-moj@users.noreply.github.com>
2024-10-28 09:31:38 +0000

    Create request/response models (#51)
    * Create a base request model
    * Create response models

--- app/models/base.py
+++ app/models/base.py
@@ -1,7 +1,17 @@
+import uuid
+from functools import cached_property
+from typing import List, Tuple, Any
+
 from sqlalchemy.orm import declared_attr
+from sqlalchemy.inspection import inspect
 from sqlmodel import Field, SQLModel
 from datetime import datetime, UTC
-from uuid import UUID, uuid4
+from pydantic import BaseModel
+from pydantic.config import ConfigDict
+
+
+def generate_id() -> uuid.UUID:
+    return uuid.uuid4()
 
 
 class TimestampMixin(SQLModel):
@@ -27,7 +37,7 @@ class TableModelMixin(TimestampMixin):
     """Mixin adding a UUID4 ID and Timestamps to child models.
     All models which exist in the database should inherit from this mixin"""
 
-    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    id: uuid.UUID = Field(default_factory=generate_id, primary_key=True)
 
     @declared_attr
     def __tablename__(self) -> str:
@@ -40,3 +50,72 @@ class TableModelMixin(TimestampMixin):
         match their type annotations"""
 
         validate_assignment = True
+
+
+class BaseResponse(TableModelMixin):
+    model_config = ConfigDict(from_attributes=True)
+    pass
+
+
+class BaseRequest(BaseModel):
+    class Meta:
+        model: BaseModel
+
+    @property
+    def model(self) -> BaseModel:
+        if not self.Meta.model:
+            raise NotImplementedError(
+                "Either set the Meta.model property or override the get_model() method."
+            )
+        return self.Meta.model
+
+    @cached_property
+    def related_fields(self) -> List[str]:
+        related_fields = []
+        model = self.Meta.model
+        if not model:
+            return related_fields
+        relationship_list: Tuple[str, Any] = inspect(model).relationships.items()
+        for relationship in relationship_list:
+            field_name = relationship[0]
+            if field_name in self.model_fields:
+                related_fields.append(field_name)
+        return related_fields
+
+    def translate(self) -> dict:
+        """
+        Convert a dump of request to a dict that can easily be used to create an instance of a model
+
+        This method currently only translates relationships that are two levels deep i.e Case.person
+        and not Case.person.income
+
+        Todo: Allow deeper level of nested fields to be translated
+        """
+        data = self.model_dump()
+        related_fields_names = self.related_fields
+        related_fields_data = {}
+        fields = {}
+        for field_name, field_value in data.items():
+            if field_name in related_fields_names:
+                related_fields_data[field_name] = field_value
+            else:
+                fields[field_name] = field_value
+        return {**fields, **self._translate_related_fields(related_fields_data)}
+
+    def _translate_related_fields(self, fields) -> dict:
+        """Convert related fields from a dict to an instance of their declared model"""
+        instances = {}
+        for field_name, field_value in fields.items():
+            field = getattr(self, field_name)
+            if not field:
+                continue
+            if isinstance(field, list):
+                model = field[0].model
+            else:
+                model = field.model
+
+            if isinstance(field_value, list):
+                instances[field_name] = [model(**value) for value in field_value]
+            else:
+                instances[field_name] = model(**field_value)
+        return instances
--- app/models/cases.py
+++ app/models/cases.py
@@ -1,20 +1,19 @@
-from sqlmodel import Field, SQLModel, Relationship
+from sqlmodel import Field, Relationship
 from typing import List
-from app.models.base import TableModelMixin
+from app.models.base import TableModelMixin, BaseRequest, BaseResponse
 from app.models.types.case_types import CaseTypes
-from app.models.case_notes import CaseNote
-from app.models.person import Person
-from app.models.case_tracker import CaseTracker
-from app.models.eligibility_outcomes import EligibilityOutcomes
+from app.models.case_notes import CaseNote, CaseNotesRequest, CaseNotesResponse
+from app.models.person import Person, PersonRequest, PersonResponse
+from app.models.case_tracker import CaseTracker, CaseTrackerRequest, CaseTrackerResponse
+from app.models.eligibility_outcomes import (
+    EligibilityOutcomes,
+    EligibilityOutcomesRequest,
+    EligibilityOutcomesResponse,
+)
 
 
-class BaseCase(SQLModel):
-    case_type: CaseTypes = Field(
-        index=True
-    )  # Which service is the case originally from
-
-
-class Case(BaseCase, TableModelMixin, table=True):
+class Case(TableModelMixin, table=True):
+    case_type: CaseTypes = Field(index=True)
     # Cascade delete ensures all related fields are deleted when the attached case is deleted.
     notes: List[CaseNote] = Relationship(back_populates="case", cascade_delete=True)
     people: List[Person] = Relationship(back_populates="case", cascade_delete=True)
@@ -24,7 +23,19 @@ class Case(BaseCase, TableModelMixin, table=True):
     )
 
 
-class CaseRequest(BaseCase):
-    """Request model used to create a new case"""
+class CaseRequest(BaseRequest):
+    case_type: CaseTypes
+    notes: List[CaseNotesRequest] | None
+    people: List[PersonRequest] | None
+    case_tracker: CaseTrackerRequest | None
+    eligibility_outcomes: List[EligibilityOutcomesRequest] | None
 
-    pass
+    class Meta(BaseRequest.Meta):
+        model = Case
+
+
+class CaseResponse(BaseResponse):
+    notes: List[CaseNotesResponse] | None
+    people: List[PersonResponse] | None
+    case_tracker: CaseTrackerResponse | None
+    eligibility_outcomes: List[EligibilityOutcomesResponse] | None

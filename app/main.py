7d576020b0c9472f60f652f4fae151008fe8826f
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2024-12-18 15:46:12 +0000

    Add in v1

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
-from .routers import case_information, security
+from fastapi import FastAPI, APIRouter
+from .routers.v1 import case_information as case_information_v1, security as security_v1
 from .config.docs import config as docs_config
+
+
+def v1_router():
+    router = APIRouter(prefix="/v1")
+    router.include_router(case_information_v1.router)
+    router.include_router(security_v1.router)
+    return router
+
+
+def v2_router():
+    router = APIRouter(prefix="/v2")
+    router.include_router(case_information_v1.router)
+    return router
 
 
 def create_app() -> FastAPI:
     app = FastAPI(**docs_config)
-    app.include_router(case_information.router)
-    app.include_router(security.router)
+    app.include_router(v1_router())
 
     return app
